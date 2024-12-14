53a1b220fbada7e24a1b6a26cc76e346a1762c4a
AbdulkadirKilic <kilicabdulkadir32@gmail.com>
2024-12-14 14:57:57 +0300

    Added solution for LeetCode problem: Increasing Triplet Subsequence

---
+++ src/solutions/LeetCode_75/Increasing_Triplet_Subsequence/Solution.java
@@ -0,0 +1,21 @@
+// LeetCode Question: https://leetcode.com/problems/increasing-triplet-subsequence
+
+package solutions.LeetCode_75.Increasing_Triplet_Subsequence;
+
+class Solution {
+  public boolean increasingTriplet(int[] nums) {
+    int first = Integer.MAX_VALUE, second = Integer.MAX_VALUE;
+
+    for (int num : nums) {
+      if (num <= first) {
+        first = num; // Update the smallest number
+      } else if (num <= second) {
+        second = num; // Update the second smallest number
+      } else {
+        return true; // A third number greater than 'first' and 'second' is found
+      }
+    }
+
+    return false; // No increasing triplet subsequence found
+  }
+}
