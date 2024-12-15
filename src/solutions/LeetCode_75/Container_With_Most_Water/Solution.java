c35d7796d1f4019ffafc2413d289159f14a941d5
AbdulkadirKilic <kilicabdulkadir32@gmail.com>
2024-12-15 11:29:53 +0300

    Added solution for LeetCode problem: Container With Most Water

---
+++ src/solutions/LeetCode_75/Container_With_Most_Water/Solution.java
@@ -0,0 +1,24 @@
+// LeetCode Question: https://leetcode.com/problems/container-with-most-water
+
+package solutions.LeetCode_75.Container_With_Most_Water;
+
+class Solution {
+  public int maxArea(int[] height) {
+    int left = 0, right = height.length - 1;
+    int maxArea = 0;
+
+    while (left < right) {
+      int currentHeight = Math.min(height[left], height[right]);
+      int width = right - left;
+      maxArea = Math.max(maxArea, currentHeight * width);
+
+      if (height[left] < height[right]) {
+        left++;
+      } else {
+        right--;
+      }
+    }
+
+    return maxArea;
+  }
+}
