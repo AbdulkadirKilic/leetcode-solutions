ad8a2c48e3903d7045863ad8cdeebd96e453328b
AbdulkadirKilic <kilicabdulkadir32@gmail.com>
2025-01-19 12:50:20 +0300

    Added solution for LeetCode problem: Group Anagrams

---
+++ src/solutions/Array/Group_Anagrams/Solution.java
@@ -0,0 +1,22 @@
+// LeetCode Question: https://leetcode.com/problems/group-anagrams
+
+package solutions.Array.Group_Anagrams;
+
+import java.util.*;
+
+class Solution {
+  public List<List<String>> groupAnagrams(String[] strs) {
+    Map<String, List<String>> anagramMap = new HashMap<>();
+
+    for (String str : strs) {
+      char[] charArray = str.toCharArray();
+      Arrays.sort(charArray);
+      String sortedStr = new String(charArray);
+
+      anagramMap.putIfAbsent(sortedStr, new ArrayList<>());
+      anagramMap.get(sortedStr).add(str);
+    }
+
+    return new ArrayList<>(anagramMap.values());
+  }
+}
